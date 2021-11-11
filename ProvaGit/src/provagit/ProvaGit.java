e1aa97dcb11ed7c34bdfe65b206f6def2dfccec0
lucia <lucia@host.docker.internal>
2021-11-11 11:36:07 +0100

    Secondo commit

--- ProvaGit/src/provagit/ProvaGit.java
+++ ProvaGit/src/provagit/ProvaGit.java
@@ -17,6 +17,7 @@ public class ProvaGit {
     public static void main(String[] args) {
       
         System.out.println("Hello, world");
+        System.out.println("Hello, universe");
     }
     
 }
